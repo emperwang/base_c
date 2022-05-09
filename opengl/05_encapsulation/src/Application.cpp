ca8c6b6af56f4b881a1394e1f78b0624bb26c832
emperWang <544094478@qq.com>
2022-05-09 19:30:00 +0800

    restructure application.cpp

--- opengl/05_encapsulation/src/Application.cpp
+++ opengl/05_encapsulation/src/Application.cpp
@@ -25,49 +25,22 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void process_input(GLFWwindow *window);
 
-void main() {
+GLFWwindow* GLfwInit();
 
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+void ImGuiInit(GLFWwindow* window);
 
-#ifdef _MAC_
-	glfwWindowHin(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif // _MAC_
+void ImGuiClear();
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return ;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+void GLfwClear(GLFWwindow* window);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		while (1);
-		return;
-	}
-	
-	glfwSwapInterval(1);
-
+void main() 
+{
+	GLFWwindow* window = GLfwInit();
 	// enable blend
 	GLCall(glEnable(GL_BLEND));
 	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-	// setup ImGui
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-	ImGui::StyleColorsDark();
-
-	//SetUp Platform/Renderer back ends
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	const char* glsl_version = "#version 130";
-	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImGuiInit(window);
 
 	{
 		GLCall(glPolygonMode(GL_FRONT_AND_BACK, GL_FILL));
@@ -78,12 +51,11 @@ void main() {
 
 		while (!glfwWindowShouldClose(window)) {
 			process_input(window);
-
-
 			// start ImGui NewFrame
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
+
 			//testColor.OnRender();
 			//testColor.OnImGuiRender();
 			testMoveModel.OnRender();
@@ -98,14 +70,69 @@ void main() {
 		}
 	}
 
+	ImGuiClear();
+	GLfwClear(window);
+	return;
+}
+
+GLFWwindow* GLfwInit()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef _MAC_
+	glfwWindowHin(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif // _MAC_
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		while (1);
+		return nullptr;
+	}
+
+	glfwSwapInterval(1);
+	return window;
+}
+
+void ImGuiInit(GLFWwindow* window)
+{
+	// setup ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	ImGui::StyleColorsDark();
+
+	//SetUp Platform/Renderer back ends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	const char* glsl_version = "#version 130";
+	ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
+void ImGuiClear()
+{
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
+}
 
+void GLfwClear(GLFWwindow* window)
+{
 	glfwDestroyWindow(window);
 	glfwTerminate();
-	return;
 }
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
