fd64b1a6761c64258f5c9c4b103a7c33c631ee22
emperWang <544094478@qq.com>
2022-08-19 15:09:58 +0800

    select socket

--- unix-c/select/select_socket.cpp
+++ unix-c/select/select_socket.cpp
@@ -8,24 +8,19 @@
 #include <vector>
 #include <iostream>
 #include <errno.h>
+#include <stdio.h>
 #include <string.h>
 #define INVALIDE_FD -1
 
-int listenfd;
-int* initialize()
+void initialize(int* listenfd)
 {
      // create a new socket
-     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listenfd == INVALIDE_FD)
-    {
-        std::cout<<("create listen socket failed.") << std::endl;
-        return NULL;
-    }
-
-    return &listenfd;
+    *listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    int val=1;
+    setsockopt(*listenfd, SOL_SOCKET, SO_REUSEADDR,&val, sizeof(val));
 }
 
-int bindaddr()
+int bindaddr(int* listenfd)
 {
     /*
     struct sockaddr_in
@@ -54,35 +49,37 @@ int bindaddr()
     bindaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     bindaddr.sin_port = htons(10000);
 
-    if(bind(listenfd, (struct sockaddr *)&bindaddr, sizeof(bindaddr)) == -1)
+    if(bind((*listenfd), (struct sockaddr *)&bindaddr, sizeof(bindaddr)) == -1)
     {
-        std::cout<< "bind listenfd failed. errno: "<< errno << std::endl;
-        close((listenfd));
+        int error = errno;
+        std::cout<< "bind listenfd failed. errno: "<< error<< std::endl;
+        perror("error: ");
+        close((*listenfd));
         return -1;
     }
     return 0;
 }
 
-int listensocket()
+int listensocket(int* listenfd)
 {
 
     // start listen
-    if(listen(listenfd, SOMAXCONN) == -1)
+    if(listen((*listenfd), SOMAXCONN) == -1)
     {
         std::cout<< "listen error." << std::endl;
-        close(listenfd);
+        close((*listenfd));
         return -1;
     }
 
     return 0;
 }
 
-void setreadset(fd_set& readset,int& maxfd, std::vector<int>& clientfds)
+void setreadset(int* listenfd,fd_set& readset,int& maxfd, std::vector<int>& clientfds)
 {
     FD_ZERO(&readset);
 
     // set listenfd into readset
-    FD_SET((listenfd), &readset);
+    FD_SET((*listenfd), &readset);
 
 
 
@@ -107,19 +104,20 @@ void setreadset(fd_set& readset,int& maxfd, std::vector<int>& clientfds)
 // use c++, because i want to use STD library.
 int main(int argc, char *argv[])
 {
-   int* listenfd = initialize();
-   if(listenfd == NULL)
-   {
-        std::cout << "create listen socket failed. errno " << errno <<std::endl;
+   int listenfd;
+   initialize(&listenfd);
+   if(listenfd == INVALIDE_FD)
+    {
+        std::cout<<("create listen socket failed.") << std::endl;
         return -1;
-   }
+    }
 
-    if(bindaddr() < 0)
+    if(bindaddr(&listenfd) < 0)
     {
         return -1;
     }
 
-    if(listensocket() < 0)
+    if(listensocket(&listenfd) < 0)
     {
         return -1;
     }
@@ -127,11 +125,11 @@ int main(int argc, char *argv[])
     // storage receive client key
     std::vector<int> clientfds;
     int maxfd;
-    maxfd = (*listenfd);
+    maxfd = listenfd;
     while(true)
     {
         fd_set readset;
-        setreadset(readset, maxfd,clientfds);
+        setreadset(&listenfd,readset, maxfd,clientfds);
         timeval tm;
         tm.tv_sec = 1;
         tm.tv_usec = 0;
@@ -150,12 +148,12 @@ int main(int argc, char *argv[])
             continue;
         }else {
             // receive read event, socket event
-            if(FD_ISSET(*listenfd, &readset))
+            if(FD_ISSET(listenfd, &readset))
             {
                 struct sockaddr_in  clientaddr;
                 socklen_t addrlen = sizeof(clientaddr);
                 // receive client
-                int clientfd = accept(*listenfd,(struct sockaddr *)&clientaddr,&addrlen);
+                int clientfd = accept(listenfd,(struct sockaddr *)&clientaddr,&addrlen);
                 if(clientfd == INVALIDE_FD)
                 {
                     // reiceve socket error
@@ -175,10 +173,20 @@ int main(int argc, char *argv[])
                     {
                         memset(recvbuf, 0, sizeof(recvbuf));
                         //receive client data
+                        // 接收数据返回0, 表明对端关闭了连接
                         int datalength = recv(clientfds[i],recvbuf,4096, 0);
                         if(datalength < 0)
                         {
                             std::cout << "recv data error, clientfd = " << clientfds[i] << std::endl;
+                            close(clientfds[i]);
+                            // mark the fd as invalid_fd
+                            clientfds[i] = INVALIDE_FD;
+                            continue;
+                        }
+
+                        if(datalength == 0)
+                        {
+                            std::cout << "client close, clientfd = " << clientfds[i] << std::endl;
                             close(clientfds[i]);
                             // mark the fd as invalid_fd
                             clientfds[i] = INVALIDE_FD;
@@ -201,7 +209,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    close((*listenfd));
+    close(listenfd);
     return 0;
 }
 
